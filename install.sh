1bccc8db79d56e68e1947a57c3eb6172ca36cfe6
Joe DiPilato <joe.dipilato@mongodb.com>
2024-04-18 13:20:33 -0400

    fix paths

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-"${SCRIPT_DIR}"/brew_install.sh
-"${SCRIPT_DIR}"/custom_install.sh
+"${SCRIPT_DIR}"/packages/brew_install.sh
+"${SCRIPT_DIR}"/packages/custom_install.sh
