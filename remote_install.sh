b10061fced835379b56d2e7e2acbcc4907e0af59
Joe DiPilato <joe.dipilato@mongodb.com>
2024-04-18 13:15:44 -0400

    update

--- remote_install.sh
+++ remote_install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 REPO_DIR=$(mktemp -d -t mac-dotfiles-XXXXXXXXXX)
-git clone --depth=1 git@github.com:joe-dipilato/mac-dotfiles.git "${REPO_DIR}"
+git clone --depth=1 git@github.com:joe-dipilato/mac-dotfiles.git "${REPO_DIR}/mac-dotfiles"
 source "${REPO_DIR}"/install.sh
-rm -rf "${REPO_DIR}"
+# rm -rf "${REPO_DIR}"
